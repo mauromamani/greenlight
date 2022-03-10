ed0e4086f92ee66c9b24a114f530dc937d6c25e4
mauromamani <mauromamani.b@gmail.com>
2022-03-09 23:10:51 -0300

    chore: add movie handlers

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,6 +12,8 @@ func (app *application) routes() *httprouter.Router {
 
 	// Register endpoints
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
 	return router
 }
