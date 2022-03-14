5fc64b087617a040fcc090bcf3d3afa209f8acd2
mauromamani <mauromamani.b@gmail.com>
2022-03-13 21:47:08 -0300

    chore: create movie model

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/mauromamani/greenlight/internal/validator"
@@ -28,4 +29,28 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
 	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
 	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
+type MovieModel struct {
+	DB *sql.DB
+}
+
+// Insert:
+func (m *MovieModel) Insert(movie *Movie) error {
+	return nil
+}
+
+// Get:
+func (m *MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+// Update:
+func (m *MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+// Delete:
+func (m *MovieModel) Delete(id int64) error {
+	return nil
 }
