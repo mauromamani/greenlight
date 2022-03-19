63960b1c1e0759bed63ab875d7e4b58d97ab96da
mauromamani <mauromamani.b@gmail.com>
2022-03-19 16:01:59 -0300

    chore: add graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"time"
 
@@ -76,23 +73,7 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	// Declare a HTTP server with config
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		ErrorLog:     log.New(logger, "", 0),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
-
-	// Start HTTP server
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-
-	err = srv.ListenAndServe()
+	err = app.serve()
 	logger.PrintFatal(err, nil)
 }
 
