a213567dd81153c01439d822667a94a144eef630
mauromamani <mauromamani.b@gmail.com>
2022-03-21 17:11:54 -0300

    chore: send emails

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/mauromamani/greenlight/internal/data"
 	"github.com/mauromamani/greenlight/internal/jsonlog"
+	"github.com/mauromamani/greenlight/internal/mailer"
 )
 
 const version = "1.0.0"
@@ -28,12 +29,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -42,15 +51,24 @@ func main() {
 	// Read the value of the port and env
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	//DB
+
+	// Database
 	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:1234@localhost/greenlight", "PostgreSQL DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
 	// Rate limit
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
+	// SMTP server
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "a0cc4980e88c1d", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "4507b3d560b4ac", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.net>", "SMTP sender")
 
 	flag.Parse()
 
@@ -71,6 +89,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
--- go.mod
+++ go.mod
@@ -8,4 +8,8 @@ require github.com/lib/pq v1.10.4
 
 require golang.org/x/time v0.0.0-20220224211638-0e9765cccd65
 
-require golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
+require (
+	github.com/go-mail/mail/v2 v2.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
